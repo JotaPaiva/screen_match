b225f781805259be7748b7f658133b9e8f54d7f1
JotaPaiva <jonataspaiva2002@hotmail.com>
2023-05-05 19:11:10 -0300

    Curso de APIs, gravação de arquivos e exceções.

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Qual filme você gostaria de buscar: ");
+        var nome = scanner.nextLine();
+
+        String endereco = "https://omdbapi.com/?t=" + nome + "&apikey=d0bf249";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+    }
+
+}
